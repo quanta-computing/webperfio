639d0df0db1b9ef3e947045d34cc5823ab4b6916
Anh-Tuan Gai <atg@basilic.io>
2015-02-11 21:25:36 +0100

    Added lazy loading of carousel images

--- _assets/javascripts/webperfio.js
+++ _assets/javascripts/webperfio.js
@@ -1,3 +1,10 @@
 //= require jquery.1.11.0.min.js
 //= require bootstrap.min.js
-//= require jquery.scrollTo.min.js
+//= require lazy-bootstrap-carousel-v3.js
+
+$(document).ready(function() {
+  //Start carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+});
