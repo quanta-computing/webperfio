53ddc690f86d5d330c07d56dbc28eeb6fe08336b
Anh-Tuan Gai <atg@basilic.io>
2015-02-11 21:36:08 +0100

    solve the lazy loading problem on carousel

--- _assets/javascripts/webperfio.js
+++ _assets/javascripts/webperfio.js
@@ -1,2 +1,10 @@
 //= require jquery.1.11.0.min.js
 //= require bootstrap.min.js
+//= require lazy-bootstrap-carousel-v3.js
+
+$(document).ready(function() {
+  //Start carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+});
