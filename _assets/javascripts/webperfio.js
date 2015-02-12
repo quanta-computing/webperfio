ae44ee9d21eb6734301eb0a6edd02589df3e64e9
Anh-Tuan Gai <atg@basilic.io>
2015-02-12 09:41:39 +0100

    Added magnified popup for screenshots on product page

--- _assets/javascripts/webperfio.js
+++ _assets/javascripts/webperfio.js
@@ -1,10 +1,23 @@
 //= require jquery.1.11.0.min.js
 //= require bootstrap.min.js
 //= require lazy-bootstrap-carousel-v3.js
+// = require jquery.magnific-popup.min.js
 
 $(document).ready(function() {
   //Start carousel
   $('.carousel').carousel({
     interval: 5000
+  });
+  $(document).ready(function() {
+    $('.carousel-inner').magnificPopup({
+      delegate: '.magnified',
+      type: 'image',
+      gallery: {
+        enabled: true
+      },
+      image: {
+        titleSrc: 'title'
+      }
+    });
   });
 });
